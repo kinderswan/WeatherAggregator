6cf9be052678afda02729068957bc98f45ccb1a3
kinderswan <gamanovichpavel@gmail.com>
2016-07-28 18:01:52 +0300

    WebRequestor methods added

---
+++ WeatherAggregator.Rest/HttpMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAggregator.Rest
+{
+	public enum HttpMethod:byte
+	{
+		Get,
+		Post,
+		Put,
+		Delete
+	}
+}
--- WeatherAggregator.Website/Scripts/WeatherAggregatorApp/App/app.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../models/weatherInfo.ts" />
-var WeatherAggregatorApp = (function () {
-    function WeatherAggregatorApp() {
-        this.mainApp = {};
-    }
-    return WeatherAggregatorApp;
-})();
-//# sourceMappingURL=app.js.map
