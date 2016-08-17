6feebffaa5aefcdba1a77d50e902a31a5d36b46d
kinderswan <gamanovichpavel@gmail.com>
2016-08-17 15:02:36 +0300

    Add tests

--- WeatherAggregator.Rest.Tests/TestResponse.cs
+++ WeatherAggregator.Rest.Tests/TestResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WeatherAggregator.Rest.Tests
+﻿namespace WeatherAggregator.Rest.Tests
 {
 	public class TestResponse
 	{
